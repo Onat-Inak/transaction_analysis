4a57335b3da3ec198f372a75f9dcc82004af5790
Onat Inak <onat.inak@gmail.com>
2024-07-03 14:50:27 +0200

    update

--- check_cuda.py
+++ check_cuda.py
@@ -6,10 +6,13 @@ cuda_available = torch.cuda.is_available()
 if cuda_available:
     # Print the number of available GPUs
     print(f"CUDA is available! \nNumber of GPUs: {torch.cuda.device_count()}")
+    
     # Get the current CUDA device
     current_device = torch.cuda.current_device()
+    
     # Get the name of the current device
     device_name = torch.cuda.get_device_name(current_device)
     print(f"Current CUDA device: {current_device} ({device_name})")
+    
 else:
     print("CUDA is not available!")
